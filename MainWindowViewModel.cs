51297bb2a4ce0596e3040b5496477a5782298e17
再頒布可能幼女 <redistributable@example.com>
2017-12-14 15:58:27 +0900

    追加

---
+++ MainWindowViewModel.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+using LocusCommon.Windows.ViewModels;
+
+using WPFDialogSample.Samples;
+
+namespace WPFDialogSample
+{
+    public class MainWindowViewModel : ExtendedViewModelBase
+    {
+        // 非公開フィールド
+        private DelegateCommand sampleButton1Command;
+        private DelegateCommand sampleButton2Command;
+        private DelegateCommand sampleButton3Command;
+
+
+        // 公開プロパティ :: バインディングプロパティ
+
+        public string SampleDialogResultText
+        {
+            get => this.GetBindingValue<string>();
+            set => this.SetBindingValueQuick(value);
+        }
+
+
+        // 公開プロパティ :: コマンド
+
+        public ICommand SampleButton1Command
+        {
+            get => this.sampleButton1Command;
+        }
+
+        public ICommand SampleButton2Command
+        {
+            get => this.sampleButton2Command;
+        }
+
+        public ICommand SampleButton3Command
+        {
+            get => this.sampleButton3Command;
+        }
+
+
+        // コンストラクタ
+
+        /// <summary>
+        /// 新しい MainWindowViewModel クラスのインスタンスを初期化します．
+        /// </summary>
+        public MainWindowViewModel()
+        {
+            this.SampleDialogResultText = "Please click any button.";
+
+            this.sampleButton1Command = new DelegateCommand(param => this.SampleDialogResultText = SimpleDialogSample.ShowSampleDialog());
+            this.sampleButton2Command = new DelegateCommand(param => { });
+            this.sampleButton3Command = new DelegateCommand(param => { });
+        }
+    }
+}
---
+++ Samples/SimpleDialogSample.xaml.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WPFDialogSample.Samples
+{
+    // コードビハインドに直接記述していく方式のダイアログ
+
+    /// <summary>
+    /// SimpleDialogSample.xaml の相互作用ロジック
+    /// </summary>
+    public partial class SimpleDialogSample : Window
+    {
+        /// <summary>
+        /// 新しい SimpleDialogSample クラスのインスタンスを初期化します．
+        /// </summary>
+        public SimpleDialogSample()
+        {
+            InitializeComponent();
+        }
+
+
+        // 静的公開メソッド
+
+        /// <summary>
+        /// サンプルダイアログを表示します．
+        /// </summary>
+        /// <returns>クリックされたボタンのテキスト</returns>
+        public static string ShowSampleDialog()
+        {
+            var instance = new SimpleDialogSample();
+            string result = null;
+            RoutedEventHandler clickAction = (sender, e) =>
+            {
+                // ボタンがクリックされたときのアクションを定義
+                // クリックされたボタンのテキストを取得
+                result = ((Button)sender).Content as string;
+                instance.Close();
+            };
+
+            // 配置されているすべてのボタンにさっき定義したアクションを関連付け
+            foreach (var button in instance.mainStackPanel.Children.OfType<Button>())
+                button.Click += clickAction;
+
+            instance.ShowDialog();
+            return result;
+        }
+    }
+}
